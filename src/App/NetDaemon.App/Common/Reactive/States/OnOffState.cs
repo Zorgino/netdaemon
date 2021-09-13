a5cb0004f139d54b1cf932cf6eb6e433d1c3c878
Zorgino <maxsu4@gmail.com>
2021-09-13 22:20:03 +0300

    add

---
+++ src/App/NetDaemon.App/Common/Reactive/States/OnOffState.cs
@@ -0,0 +1,34 @@
+﻿namespace NetDaemon.Common.Reactive.States
+{
+    public class OnOffState : StateValueObject<bool?>
+    {
+        public OnOffState(object? state) : base(state)
+        {
+        }
+
+        public virtual bool IsOn => Value == true;
+
+        public virtual bool IsOff => Value == false;
+
+        protected override bool? ConvertToValue()
+        {
+            return State switch{
+                "on" => true,
+                "off" => false,
+                _ => null
+            };
+        }
+
+        public override bool IsMissing => base.IsMissing || Value is null;
+
+        public static implicit operator bool(OnOffState state)
+        {
+            return !state.IsMissing && state.IsOn;
+        }
+
+        public static implicit operator bool?(OnOffState state)
+        {
+            return state.Value;
+        }
+    }
+}
